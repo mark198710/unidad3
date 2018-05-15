a3188877b4e262d513a927197fb2c2229cb46b39
marco <marcoavegagon@gmail.com>
2018-05-15 13:56:07 -0500

    cambios2

--- Unidad3/Unidad3/Program.cs
+++ Unidad3/Unidad3/Program.cs
@@ -10,7 +10,7 @@ namespace Unidad3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hola1.2");
+            Console.WriteLine("Hola2");
         }
     }
 }
